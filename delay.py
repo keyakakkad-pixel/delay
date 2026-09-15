28eefedb8fee291b8f017cf829f8be084079c0e9
keyakakkad-pixel <keyakakkad@gmail.com>
2026-09-15 17:27:03 +0530

    Update delay.py

--- delay.py
+++ delay.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 import joblib
-
+import pickle
 # Load the trained model
 filename = r'logistic_model.sav'
-loaded_model = joblib.load(open(filename, 'rb'))
+loaded_model = pickle.load(open(filename, 'rb'))
 
 # Define the correct column names
 columns = ['Delivery_Distance', 'Traffic_Congestion', 'Weather_Condition',
